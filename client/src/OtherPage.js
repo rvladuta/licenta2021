3c44b6c7d79eead04bb725a041c44bffe04600bb
Razvan2x <vldt_razvan@yahoo.com>
2021-05-30 14:04:09 +0300

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <br></br>
+          <br></br>
+          <br></br>
+      Cealalta pagina!
+      <Link to="/">Inapoi acasa</Link>
+      <br></br>
+          <br></br>
+          <br></br>
+    </div>
+  );
+};
